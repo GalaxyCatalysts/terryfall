2bcab0e2050d552b1ca5db195d2c59477b5bcbe4
Plaonder Joander <biz@jayden.phindog.family>
2021-08-05 01:00:31 -0700

    Comment cleanup from base Minimal

--- code/Game.cs
+++ code/Game.cs
@@ -5,24 +5,10 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 
-//
-// You don't need to put things in a namespace, but it doesn't hurt.
-//
 namespace Terryfall
 {
-
-	/// <summary>
-	/// This is your game class. This is an entity that is created serverside when
-	/// the game starts, and is replicated to the client. 
-	/// 
-	/// You can use this to create things like HUDs and declare which player class
-	/// to use for spawned players.
-	/// 
-	/// Your game needs to be registered (using [Library] here) with the same name 
-	/// as your game addon. If it isn't then we won't be able to find it.
-	/// </summary>
 	[Library( "terryfall" )]
-	public partial class TerryfallGame : Sandbox.Game
+	public partial class TerryfallGame : Game
 	{
 		public TerryfallGame()
 		{
@@ -30,10 +16,7 @@ namespace Terryfall
 			{
 				Log.Info( "Server created" );
 
-				// Create a HUD entity. This entity is globally networked
-				// and when it is created clientside it creates the actual
-				// UI panels. You don't have to create your HUD via an entity,
-				// this just feels like a nice neat way to do it.
+				// Create a HUD entity and code based HUD
 				new THudEntity();
 				new THUD();
 			}
@@ -45,12 +28,13 @@ namespace Terryfall
 		}
 
 		/// <summary>
-		/// A client has joined the server. Make them a pawn to play with
+		/// A client has joined the server. Make them a pawn
 		/// </summary>
 		public override void ClientJoined( Client client )
 		{
 			base.ClientJoined( client );
 
+			// Give the player their player
 			var player = new TPlayer();
 			client.Pawn = player;
 
